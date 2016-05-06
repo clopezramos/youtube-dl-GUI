6104816bebb7f138c8a61630d38cbf30cff7744e
clopezramos <clopezdist@gmail.com>
2016-05-06 09:25:32 +0200

    base project

---
+++ Main.pas
@@ -0,0 +1,24 @@
+unit MainUnit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
